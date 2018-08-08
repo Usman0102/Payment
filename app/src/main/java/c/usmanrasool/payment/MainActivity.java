b707266858fb971f7dd1bec8059968b67d3f8416
Usman Rasool <usmanrasoolbhatti@gmail.com>
2018-08-08 03:05:07 -0700

    Edited

--- app/src/main/java/c/usmanrasool/payment/MainActivity.java
+++ app/src/main/java/c/usmanrasool/payment/MainActivity.java
@@ -1,13 +1,33 @@
 package c.usmanrasool.payment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnUsman;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnUsman = (Button) findViewById(R.id.usmanBtn);
+
+        btnUsman.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(MainActivity.this, usman.class);
+                startActivity(i);
+
+            }
+        });
+
+
+
     }
 }
---
+++ app/src/main/java/c/usmanrasool/payment/usman.java
@@ -0,0 +1,17 @@
+package c.usmanrasool.payment;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Usman Rasool on 8/8/2018.
+ */
+
+public class usman extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.usman);
+    }
+}
